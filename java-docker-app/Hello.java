bfbf8cdbd17b9df53af7adff88d478653671fa65
mahammadalthaf <shaikmahammadalthaf1997@gmail.com>
2021-07-29 01:50:35 -0700

    3 files added

---
+++ java-docker-app/Hello.java
@@ -0,0 +1,8 @@
+    class Hello{  
+    public static void main(String[] args){  
+    System.out.println("SUM OF NUMBERS");  
+    int n1=5;
+    int n2=100;
+    System.out.println(n1+ " and "+ n2+" is "+(n1+n2));
+    } 
+    } 
